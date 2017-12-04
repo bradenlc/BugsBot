6161fc6390868ede1ee564bfabf023ce71953fa4
BradyC125 <bradycrimmins@gmail.com>
2017-12-04 17:33:42 -0500

    Transfer relevant functions to SH.py

--- SH.py
+++ SH.py
@@ -1,1 +1,18 @@
+import discord
+import random
+import asyncio
+import logging
 
+def checkIfJoined(message):
+    try:
+        message.channel.innedPlayerlist[0]
+    except NameError:
+        message.channel.innedPlayerlist = []
+    for innedPlayer in message.channel.innedPlayerlist:
+        if innedPlayer == message.author:
+            return True
+    return False
+
+def startGame(message):
+
+    
