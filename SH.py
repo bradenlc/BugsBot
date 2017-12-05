743394698b27dfdbbc1caccfddfbea52647424be
BradyC125 <bradycrimmins@gmail.com>
2017-12-04 22:54:34 -0500

    Add sendMessage(game)

--- SH.py
+++ SH.py
@@ -25,22 +25,37 @@ def addHitler(game):
     if game.hitler in game.facists:
         addHitler(game)
 
-async def assignRoles(game):
+def assignRoles(game):
     game.facists = []
     for x in range(0,numOfFacists):
         addFacist(game)
     addHitler(game)
 
+async def sendMessages(game):
+    if game.gameMode == 1:
+        await send_message(game.hitler, "You're Hitler. Your facist teammate is " + game.facists[0].name)
+        await send_message(game.facist[0], "You're a facist. Your job is to help Hitler, " + game.hitler.name)
+    elif game.gameMode == 2:
+        await send_message(game.hitler, "You're Hitler. Because you have more than one teammate, you don't get to know who they are")
+        await send_message(game.facist[0], "You're a facist. Your teammate is " + game.facist[1].name + " and Hitler is " + game.hitler.name)
+        await send_message(game.facist[1], "You're a facist. Your teammate is " + game.facist[0].name + " and Hitler is " + game.hitler.name)
+    elif game.gameMode == 3:
+        await send_message(game.facist[0], "You're a facist. Your teammates are " + game.facist[1].name + " and " + game.facist[2].name + ". Hitler is " + game.hitler.name)
+        await send_message(game.facist[1], "You're a facist. Your teammates are " + game.facist[2].name + " and " + game.facist[2].name + ". Hitler is " + game.hitler.name)
+        await send_message(game.facist[2], "You're a facist. Your teammates are " + game.facist[1].name + " and " + game.facist[0].name + ". Hitler is " + game.hitler.name)
+        await send_message(game.hitler, "You're Hitler. Your teammates are " + game.facist[0].name + ", " + game.facist[1].name + " and " game.facist[2].name)
+        
 async def startGame(message):
     game = message.channel
     game.numOfPlayers = len(message.channel.innedPlayerlist)
     if game.numOfPlayers > 8:
         game.numOfFacists = 3
-        game.gameMode = 2
+        game.gameMode = 3
     elif game.numOfPlayers > 6:
         game.numOfFacists = 2
         game.gameMode = 2
     else:
         game.numOfFacists = 1
         game.gameMode = 1
-    
+    assignRoles(game)
+    await sendMessages(game)
