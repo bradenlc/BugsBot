a79f24a441acfd38defb23c3feb0d4df5840d22c
BradyC125 <bradycrimmins@gmail.com>
2017-12-22 20:58:24 -0500

    Add affirmative and negative databanks

--- config.py
+++ config.py
@@ -1,1 +1,3 @@
 SHInstances = {}
+affirmatives = ["Yes", "yes", "YES", "Y", "y", "Yep", "yep", "!y", "!Y", "!yes", "!yes"]
+negatives = ["No", "no", "NO", "N", "n", "nope", "Nope", "nah", "Nah", "!n", "!N", "!no", "!No"]
