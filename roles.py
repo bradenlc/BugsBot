84b6221db701abf2d96adae8f109fd32a4053d3c
BradyC125 <bradycrimmins@gmail.com>
2017-12-27 21:39:15 -0500

    Add !nameme and !colorme

---
+++ roles.py
@@ -0,0 +1,103 @@
+import discord
+import asyncio
+import logging
+
+async def initRoles(message, client):
+    roleDict = {}
+    uniqueRoles = []
+    membersAndUR = {}
+    simCount = 0
+    duplicateMembers = []
+    noUniques = []
+    for r in message.server.roles:
+        roleDict[r] = 0
+    for m in message.server.members:
+        for mr in m.roles:
+            roleDict[mr] += 1
+    for r in roleDict:
+        if roleDict[r] == 1:
+            uniqueRoles.append(r)
+    for r in uniqueRoles:
+        for m in message.server.members:
+            for mr in m.roles:
+                if mr == r:
+                    if not m in membersAndUR:
+                        membersAndUR[m]=r
+                    else:
+                        duplicateMembers.append(m)
+                        membersAndUR.pop(m)
+    for m in message.server.members:
+        try:
+            membersAndUR[m]
+        except KeyError:
+            if not m in duplicateMembers:
+                noUniques.append(m)
+    print("Unique Roles: ")
+    for x in membersAndUR:
+        print(x.name + ' : ' + membersAndUR[x].name)
+    print("/nMembers with multiple unique roles: ")
+    for x in duplicateMembers:
+        print(x.name)
+    print("/nMembers without unique roles:")
+    for x in noUniques:
+        print(x.name)
+
+async def uniqueRole(message, client):
+    userRoles = []
+    for role in message.author.roles:
+        userRoles.append(role)
+    for member in message.server.members:
+        if member != message.author:
+            for memberRole in member.roles:
+                if memberRole in userRoles:
+                    userRoles.remove(memberRole)
+    try:
+        uniqueRole = userRoles[0]
+        return uniqueRole
+    except IndexError:
+        uniqueRole = await client.create_role(message.server, name = "{}'s role".format(message.author.name))
+        await client.add_roles(message.author, uniqueRole)
+        return uniqueRole
+
+async def colorMe(message, client):
+    if not message.server is None:
+        parseArray = message.content.split("#")
+        if parseArray[1].startswith("0x"):
+            parseArray[1] = parseArray[1][2:]
+        hexCode = parseArray[1][:6]
+        try:
+            colorInt = discord.Color(int(hexCode, 16))
+            member = message.author
+            memberRole = await uniqueRole(message, client)
+            await client.edit_role(message.server, memberRole, color = colorInt)
+            await client.send_message(message.channel, "Your color has been modified to hex code `#{}`".format(hexCode))
+        except ValueError:
+            await client.send_message(message.channel, "You didn't specify a valid hex code")
+        except discord.Forbidden:
+            await client.send_message(message.channel, "The bot doesn't have permission to edit your role")  
+    else:
+        await client.send_message(message.channel, "You can't run this command here")
+
+async def nameMe(message, client):
+    if not message.server is None:
+        requestedName = message.content[7:]
+        while requestedName.startswith(" "):
+            requestedName = requestedName[1:]
+        while requestedName.endswith(" "):
+            requestedName = requestedName[:len(requestedName)-1]
+        member = message.author
+        memberRole = await uniqueRole(message, client)
+        try:
+            await client.edit_role(message.server, memberRole, name = requestedName)
+            await client.send_message(message.channel, "Your role name has been set to `{}`".format(requestedName))
+        except discord.Forbidden:
+            await client.send_message(message.channel, "The bot doesn't have permission to edit your role")
+        except discord.HTTPException:
+            await client.send_message(message.channel, "The bot failed to edit your role")
+        
+
+
+
+
+
+    
