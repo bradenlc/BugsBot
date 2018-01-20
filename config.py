aef8a239f8b5c5432262724703254a823964607f
BradyC125 <bradycrimmins@gmail.com>
2018-01-20 09:54:55 -0500

    Edit config

--- config.py
+++ config.py
@@ -16,6 +16,7 @@ gameCommands = ["!gamemode",
                 "!leave",
                 "!start",
                 "!endgame",
+                "!restart", "!reset",
                 "!c", "!c1", "!c2", "!c3",
                 "!a", "!a1", "!a2", "!a3",
                 "!select",
@@ -43,3 +44,16 @@ userCommands = ["!remindme",
 
 adminCommands = ["!bedtime",
                  "!remind"]
+
+async def newGame(client, channel, requestedGamemode):
+    if requestedGamemode == "sh":
+        config.gameInstances[channel.id] = SH.SHInstance(channel, client)
+        await client.send_message(channel, "Your gamemode has been set to SH")
+    elif requestedGamemode == "villain":
+        config.gameInstances[channel.id] = superfight.Villain(channel, client)
+        await client.send_message(channel, "Your gamemode has been set to Villain Fight")
+    elif requestedGamemode == "duel":
+        config.gameInstances[channel.id] = superfight.Duel(channel, client)
+        await client.send_message(channel, "Your gamemode has been set to Duel")
+    else:
+        await client.send_message(channel, "That's not a recognized game mode")
